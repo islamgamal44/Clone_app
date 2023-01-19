d49163f9c50fe87084e3a06416207553b763f1ca
eslam gamal <eslam.gamal365.25@gmail.com>
2023-01-19 13:54:11 +0200

    update task 1

---
+++ lib/core.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class IjCircle extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _IjCircle();
+  }
+}
+
+class _IjCircle extends State<IjCircle> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController animationcontroller;
+
+  @override
+  void initState() {
+    animationcontroller =
+        AnimationController(vsync: this, duration: Duration(seconds: 1));
+
+    animationcontroller.repeat();
+
+    animation = Tween<double>(begin: 0, end: 250).animate(animationcontroller);
+
+    animation.addListener(() {
+      setState(() {});
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationcontroller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          height: 10,
+          child: Container(
+            decoration:
+                BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+            height: animation.value,
+            width: animation.value,
+          ),
+        ),
+      ),
+    );
+  }
+}
